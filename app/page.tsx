8ff48b65c0c2f3ce11a555a1edd2de2419eca62b
Ivor <lhanivor@gmail.com>
2024-05-22 09:18:59 +0800

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,16 @@ import Projects from "@/components/Section/Projects/projects";
 import SectionDivider from "@/components/Module/section-divider";
 import Skills from "@/components/Section/Skills/skills";
 
-
-
-
-
 export default function Home() {
   return (
-    
     <main className="flex flex-col items-center px-4">
-        <Intro />
-        <SectionDivider />
-        <About />
-        <Projects />
-        <Skills />
-        <Experience />
-        <Contact />
+      <Intro />
+      <SectionDivider />
+      <About />
+      <Projects />
+      <Skills />
+      <Experience />
+      <Contact />
     </main>
   );
 }
--- components/Section/Projects/projects.tsx
+++ components/Section/Projects/projects.tsx
@@ -4,26 +4,15 @@ import SectionHeading from "../../Module/section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./_Modules/project";
 import { useSectionInView } from "@/lib/hooks";
-import { useRouter } from "next/navigation";
 
 export default function Projects() {
-  const router = useRouter();
   const { ref } = useSectionInView("Projects", 0.5);
-
-  // Assuming the query parameter is 'category' to filter projects
-  const query = new URLSearchParams(window.location.search);
-  const title = query.get("title");
-  console.log(title);
-
-  const filteredProjects = title
-    ? projectsData.filter((project) => project.title === title)
-    : projectsData;
 
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My projects</SectionHeading>
       <div>
-        {filteredProjects.map((project, index) => (
+        {projectsData.map((project, index) => (
           <React.Fragment key={index}>
             <Project {...project} />
           </React.Fragment>
