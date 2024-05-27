3cc75f157691c299a2bb43df64bb15b854c71810
Ivor <lhanivor@gmail.com>
2024-05-27 20:41:41 +0800

    new commit

--- components/Section/Introduction/intro.tsx
+++ components/Section/Introduction/intro.tsx
@@ -64,7 +64,7 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I'm Mark Ivor Glorioso.</span> I'm a{" "}
         <span className="font-bold">Front-End Developer</span> with{" "}
-        <span className="font-bold">1 year</span> of experience. I enjoy
+        <span className="font-bold">3 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
